55eee915c182e52ac04193974ed302b55e86d2f6
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2021-02-14 19:37:23 -0500

    add simple game logic, add reset, add coins, jackpot, and betting

--- SlotMachine_iOS_UIKit/Controllers/ViewController.swift
+++ SlotMachine_iOS_UIKit/Controllers/ViewController.swift
@@ -36,9 +36,13 @@ class ViewController: UIViewController {
     let centerReel:Array<UIImage> = [#imageLiteral(resourceName: "icon_6"),#imageLiteral(resourceName: "icon_7"),#imageLiteral(resourceName: "icon_8"),#imageLiteral(resourceName: "icon_1"),#imageLiteral(resourceName: "icon_2"),#imageLiteral(resourceName: "icon_3"),#imageLiteral(resourceName: "icon_4"),#imageLiteral(resourceName: "icon_5")]
     let rightReel:Array<UIImage> = [#imageLiteral(resourceName: "icon_6"),#imageLiteral(resourceName: "icon_7"),#imageLiteral(resourceName: "icon_8"),#imageLiteral(resourceName: "icon_4"),#imageLiteral(resourceName: "icon_5"),#imageLiteral(resourceName: "icon_1"),#imageLiteral(resourceName: "icon_2"),#imageLiteral(resourceName: "icon_3")]
     
+    var bet = 10
+    var coins = 100
+    var jackpot = 0
+    
     var player: AVAudioPlayer?
 
-    
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -58,8 +62,57 @@ class ViewController: UIViewController {
         leftTopImage.layer.transform = topPerspectiveTransform
         centerTopImage.layer.transform = topPerspectiveTransform
         rightTopImage.layer.transform = topPerspectiveTransform
+        
+        betLabel.text = String(bet)
+        coinsLabel.text = String(coins)
+        jackpotLabel.text = String(jackpot)
     }
     
+    func reset() {
+        winJackpotImage.image = nil
+        raysJackpotImage.image = nil
+        leftFrame.alpha = 0
+        centerFrame.alpha = 0
+        rightFrame.alpha = 0
+        
+        coinsLabel.text = String(coins)
+        betLabel.text = String(bet)
+        jackpotLabel.text = String(jackpot)
+    }
+    
+    func resetEverything() {
+        let alert = UIAlertController(title: "WARNING", message: "You're about to reset the game, are you sure ?", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "YES", style: UIAlertAction.Style.default, handler: { [self] (UIAlertAction) in
+            bet = 10
+            coins = 100
+            jackpot = 0
+            reset()
+        }))
+        alert.addAction(UIAlertAction(title: "NO", style: UIAlertAction.Style.default, handler:nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    
+    }
+    
+    @IBAction func resetPressed(_ sender: UIButton) {
+        resetEverything()
+    }
+    
+    //MARK: - Betting
+    @IBAction func changeBet(_ sender: UIButton) {
+        if sender.tag == 1 {
+            bet+=1
+        }
+        else{
+            bet-=1
+        }
+        betLabel.text = String(bet)
+    }
+    
+
+    
+    //MARK: - spinning
     func animateImages(images:[UIImageView],reel:Array<UIImage>) {
         
         for (index,image) in images.enumerated() {
@@ -111,42 +164,46 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startPressed(_ sender: UIButton) {
-        reset()
-        sender.isUserInteractionEnabled = false
-        sender.setImage(#imageLiteral(resourceName: "start_active_btn"), for: .normal)
-        Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { _ in
-            sender.isUserInteractionEnabled = true
-            sender.setImage(#imageLiteral(resourceName: "start_btn"), for: .normal)
-        })
-        
-        let leftImages = [leftTopImage!,leftMiddleImage!,leftBottomImage!]
-        let centerImages = [centerTopImage!,centerMiddleImage!,centerBottomImage!]
-        let rightImages = [rightTopImage!,rightMiddleImage!,rightBottomImage!]
-        
-        playSound(soundName: "spinning.wav")
-        
-        animateImages(images: leftImages, reel: leftReel)
-        animateImages(images: centerImages, reel: centerReel)
-        animateImages(images: rightImages, reel: rightReel)
+        if coins < bet {
+            let alert = UIAlertController(title: "DENIED", message: "Insuffecient Coins, please purchase more coins to continue", preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addAction(UIAlertAction(title: "Buy 100 coin", style: UIAlertAction.Style.default, handler: { [self] (UIAlertAction) in
+                coins += 100
+                coinsLabel.text = String(coins)
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler:nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            coins = coins - bet
+            coinsLabel.text = String(coins)
+            reset()
+            sender.isUserInteractionEnabled = false
+            sender.setImage(#imageLiteral(resourceName: "start_active_btn"), for: .normal)
+            Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { _ in
+                sender.isUserInteractionEnabled = true
+                sender.setImage(#imageLiteral(resourceName: "start_btn"), for: .normal)
+            })
+            
+            let leftImages = [leftTopImage!,leftMiddleImage!,leftBottomImage!]
+            let centerImages = [centerTopImage!,centerMiddleImage!,centerBottomImage!]
+            let rightImages = [rightTopImage!,rightMiddleImage!,rightBottomImage!]
+            
+            playSound(soundName: "spinning.wav")
+            
+            animateImages(images: leftImages, reel: leftReel)
+            animateImages(images: centerImages, reel: centerReel)
+            animateImages(images: rightImages, reel: rightReel)
 
-        Timer.scheduledTimer(withTimeInterval: 2.1, repeats: false, block: { [self] _ in
-            checkWinning(leftImage: leftMiddleImage.image!, centerImage: centerMiddleImage.image!, rightImage: rightMiddleImage.image!)
-        })
-        
-    }
-    
-    func playSound(soundName: String) {
-        let path = Bundle.main.path(forResource: soundName, ofType:nil)!
+            Timer.scheduledTimer(withTimeInterval: 2.1, repeats: false, block: { [self] _ in
+                checkWinning(leftImage: leftMiddleImage.image!, centerImage: centerMiddleImage.image!, rightImage: rightMiddleImage.image!)
+            })
+            
 
-        let url = URL(fileURLWithPath: path)
-
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            player?.play()
-        } catch {
-            // couldn't load file :(
         }
     }
+
     
     func checkWinning(leftImage: UIImage, centerImage: UIImage, rightImage: UIImage){
         if leftImage == centerImage &&  leftImage == rightImage{
@@ -158,6 +215,11 @@ class ViewController: UIViewController {
             centerFrame.alpha = 1
             rightFrame.alpha = 1
             
+            
+            coins += bet*2
+            jackpot += bet*2
+            jackpotLabel.text = String(jackpot)
+            coinsLabel.text = String(coins)
             print("win")
 
         }
@@ -167,12 +229,18 @@ class ViewController: UIViewController {
             print("lose")
         }
     }
-    func reset() {
-        winJackpotImage.image = nil
-        raysJackpotImage.image = nil
-        leftFrame.alpha = 0
-        centerFrame.alpha = 0
-        rightFrame.alpha = 0
+    //MARK: - sounds
+    func playSound(soundName: String) {
+        let path = Bundle.main.path(forResource: soundName, ofType:nil)!
+
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("error loading sound file")
+        }
     }
     
 }
